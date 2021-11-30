118b9fc6ea7ef9586b4844a0de43f8b535afed2e
quinnbrussel <quinnbrussel@college.harvard.edu>
2021-11-29 19:51:15 -0500

    add more files for flask

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-print("hello, world")
-print("fuck quinn brussel")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
